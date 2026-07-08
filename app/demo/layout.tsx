b880752e304ba301400955f35dc340e69dcb6033
henriquesouza4430 <henriquesouza4430@gmail.com>
2026-07-08 01:44:12 -0300

    corrigindo erro de sintaxe no metadata

--- app/demo/layout.tsx
+++ app/demo/layout.tsx
@@ -2,10 +2,8 @@ import type { Metadata } from "next";
 import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  title: "ZH Sistema de Delivery
-Gestão de Cardápio Digital e Pedidos Online | Demonstrações",
-  description:
-    "Teste gratuitamente os planos Basic, Pro.",
+  title: "ZH Sistema de Delivery | Gestão de Cardápio Digital e Pedidos Online | Demonstrações",
+  description: "Teste gratuitamente os planos Basic, Pro.",
 };
 
 export default function DemoLayout({ children }: { children: ReactNode }) {
